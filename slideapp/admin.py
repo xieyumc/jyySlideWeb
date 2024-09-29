7abe50a6e9a58ff0dbbf8b85d67157d793786e5b
XieYu <xieyumc@gmail.com>
2024-09-29 17:45:56 +0800

    fix: 优化界面

--- slideapp/admin.py
+++ slideapp/admin.py
@@ -4,4 +4,8 @@ from django.contrib import admin
 
 from .models import Slide
 
-admin.site.register(Slide)
+
+class SlideAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'updated_at')
+
+admin.site.register(Slide, SlideAdmin)
