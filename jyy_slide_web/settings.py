e675009a3e36e74290bc09504823abc1d9dce444
XieYu <xieyumc@gmail.com>
2024-10-07 00:42:02 +0800

    fix：debug=Flase时，Django不会提供默认路由，这样上传图片会失效，所以暂时先让debug=True

--- jyy_slide_web/settings.py
+++ jyy_slide_web/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-bxd)3tz&y6oo_jq5r^92qyqbg^hx=#m(#i+n#@&cih86qmh8k1"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', 'https://localhost').split(',')
 
--- slideapp/urls.py
+++ slideapp/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('edit/<int:slide_id>/', views.edit_slide, name='edit_slide'),
     path('upload_image/', views.upload_image, name='upload_image'),
     path('delete/<int:slide_id>/', views.delete_slide, name='delete_slide'),
-# 登录和注销
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('toggle_lock/<int:slide_id>/', views.toggle_lock, name='toggle_lock'),
